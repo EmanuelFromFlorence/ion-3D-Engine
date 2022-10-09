f96eb1c532d858dfc063a169acc09e06f6ee06a7
Saman <s.sadeghy325@gmail.com>
2022-10-08 20:46:08 -0500

    three scene and gltf loading working

--- examples/web/webpack.config.js
+++ examples/web/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 
 const htmlPlugin = new HtmlWebPackPlugin({
@@ -9,26 +10,49 @@ const htmlPlugin = new HtmlWebPackPlugin({
 
 
 module.exports = {
-  entry: "./src/main.ts",
+  entry: {
+    main: './src/main.ts',
+  },
   
   output: {
     path: path.join(__dirname, 'public'),
+    // assetModuleFilename: 'assets/[name][ext]',
+    publicPath: path.join(__dirname, 'public'),
     filename: "[name].js"
   },
 
   devServer: {
     static: [path.join(__dirname, 'public')],
+    // contentBase: 'public',
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.ts'],
+    alias: { // Now, instead of using relative paths when importing ../public...
+      public: path.resolve(__dirname, 'public'),
+      resources: path.resolve(__dirname, 'resources'),
+    },
   },
 
   // Adding source map for better error logs (so not all point to bundler.js)
   devtool: 'inline-source-map',
 
   plugins: [
-    htmlPlugin
+    htmlPlugin,
+    new CopyPlugin({ // https://github.com/webpack-contrib/copy-webpack-plugin
+      patterns: [
+        { from: "resources", to: "resources" },
+      ],
+    }),
   ],
 
   module: {
     rules: [
+      // {
+      //   test: /\.html$/, 
+      //   exclude: /index\.html$/, 
+      //   loader: 'html-loader'
+      // },
       {
         test: /\.(js|jsx|ts)$/,
         exclude: /node_modules/,
@@ -43,9 +67,40 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        loader: "file-loader",
-        options: { name: 'static/[hash].[ext]' }
+        type: 'asset/resource',
+        generator: {
+          filename: '[name].[ext]?[hash]',
+        },
       },
+      {
+        test: /\.(glb|gltf|bin)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name].[ext]?[hash]',
+        },
+      },
+      // {
+      //   test: /\.(glb|gltf|bin)$/,
+      //   use:
+      //   [
+      //       {
+      //           loader: 'file-loader',
+      //           options:
+      //           {
+      //               outputPath: 'assets/models/'
+      //           }
+      //       }
+      //   ]
+      // },
+
+
+      // use: [{
+      //   loader: 'file-loader',
+      //   options: {
+      //     name: '[name].[ext]',
+      //     outputPath: 'images/'
+      //   }
+      // }]
     ]
   }
 };
